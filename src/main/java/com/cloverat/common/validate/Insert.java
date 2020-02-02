d8c88f31027b11289d9153a3afa573d609afa407
Chenzi <812944458@qq.com>
2020-02-02 14:33:31 +0800

    Test

--- src/main/java/com/cloverat/common/validate/Insert.java
+++
@@ -1,8 +0,0 @@
-package com.cloverat.common.validate;
-
-/**
- * 新增验证器
- * 
- * @author cloverat 2019/6/11
- */
-public interface Insert {}
--- src/main/java/com/cloverat/common/validate/Select.java
+++
@@ -1,8 +0,0 @@
-package com.cloverat.common.validate;
-
-/**
- * 查询验证器
- * 
- * @author cloverat 2019/6/11
- */
-public interface Select {}
